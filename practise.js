e1a3033b6a8797829e42427a1fc552673c125a28
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-06-12 15:53:15 +0100

    Initial commit

---
+++ practise.js
@@ -0,0 +1,22 @@
+/* 
+
+const greeting = 'Hello ';
+const name = 'Makers';
+
+console.log (greeting + name);
+
+const getName = () => {
+    return 'Khuslen;'
+};
+
+getName();
+
+const returnFour = () => {
+    return 4;
+  };
+  
+  const something = returnFour;
+  console.log(something);
+  */
+ 
+  const a = get
---
+++ sayHello.js
@@ -0,0 +1,12 @@
+/* const sayHello = (name) => {
+    return 'Hello' + name;
+
+};
+
+sayHello(name);
+
+*/
+
+const anothersayHello = (name) => {
+    return `Hello ${name}`
+};
