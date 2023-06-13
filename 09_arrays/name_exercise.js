8e38589ad38d7de731def23e8757bf1955bcc3e6
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-06-13 14:10:16 +0100

    09_arrays name exercise

---
+++ 09_arrays/name_exercise.js
@@ -0,0 +1,9 @@
+const names = ['Khuslen', 'Sana', 'Grace', 'Alice'];
+
+console.log(names.length);
+
+console.log(names[1]);
+
+const newNames = names.concat('Holly', 'Evie');
+
+console.log(newNames);
