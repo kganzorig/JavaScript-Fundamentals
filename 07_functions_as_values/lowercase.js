2cdd9583fcfe4bf91f3b41029a6e87cd5608b430
Khuslen Ganzorig <khusy.gan@gmail.com>
2023-06-13 11:13:05 +0100

    07 lowercase exercise

---
+++ 07_functions_as_values/lowercase.js
@@ -0,0 +1,22 @@
+const lowercaseMessage = (message) =>  {
+    return message.toLowerCase();
+}
+
+const transform = (message, transformFunction) => {
+    return transformFunction(message);
+}
+
+transform("WHY ARE YOU SHOUTING?", lowercaseMessage);
+
+module.exports = lowercaseMessage;
+
+
+/*
+Or I could've done, to print the lowercased message when running node lowercase.js:
+
+const result = transform("WHY ARE YOU SHOUTING?", lowercaseMessage);
+
+consol.log(result);
+
+*\
+
